5097203393e16a15465f86d668ede46dcbc07911
Sandeep Suri <35242894+sandeepsuri@users.noreply.github.com>
2023-03-21 14:30:41 -0400

    feat: genre search added

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -7,13 +7,21 @@ const CONSOLE = [
     "Nintendo Switch"
 ]
 
+const GENRE = [
+    "FPS", "Action", "Adventure",
+    "Sports", "Family", "RPG", "JRPG",
+    "Third Person Shooter"
+]
+
 const SearchParams = () => {
     const [game, setGame] = useState("")
     const [console, setConsole] = useState("")
+    const [genre, setGenre] = useState("")
     return (
         <div className="search-params">
             <form>
-                <label htmlFor="location">
+                {/* Game Search */}
+                <label htmlFor="game">
                     Game
                     <input 
                         onChange={(e) => setGame(e.target.value)} 
@@ -22,18 +30,37 @@ const SearchParams = () => {
                         placeholder="Game Name" 
                     />
                 </label>
-                <label htmlFor="animal">
+                {/* Console Search */}
+                <label htmlFor="console">
                     Console
                     <select 
                         id="console" 
                         value={console} 
                         onChange = {e => {
                             setConsole(e.target.value)
+                            setGenre("")
                         }}
                     >
                         <option />
                         {CONSOLE.map((console) => (
                             <option key={console}>{console}</option>
+                        ))}
+                    </select>
+                </label>
+                {/* Game Genre Search */}
+                <label htmlFor="genre">
+                    Genre
+                    <select 
+                        id="genre" 
+                        disabled={GENRE.length === 0}
+                        value={genre} 
+                        onChange = {e => {
+                            setGenre(e.target.value)
+                        }}
+                    >
+                        <option />
+                        {GENRE.map((genre) => (
+                            <option key={genre}>{genre}</option>
                         ))}
                     </select>
                 </label>
